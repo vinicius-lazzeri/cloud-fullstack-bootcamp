e475311d9c14369f6c97ced45308d21291b96ba3
Vinicius Lazzeri <vinicius.lazzeri@hotmail.com>
2022-10-31 21:05:52 -0400

    update

---
+++ arrays-e-repeticao/app.js
@@ -0,0 +1,10 @@
+const nome = 'Vinicius Costa Lazzeri da Silva';
+
+for (let i = 0; i < nome.length; i++){
+    const letra = nome [i];
+    console.log(letra);
+}
+
+for (let index = 0; index < array.length; index++){
+    const element = array[index]
+}
